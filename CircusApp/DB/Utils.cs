e89d50f2ca2d5aeeb3fe0ae41c6fe15cbc5a27e2
Mikhail Mitrofanov <mmm606@inbox.ru>
2024-11-01 16:49:37 +0300

    Добавьте файлы проекта.

---
+++ CircusApp/DB/Utils.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CircusApp.DB
+{
+    public static class Utils
+    {
+        public static circus_dbEntities DB = new circus_dbEntities();
+    }
+}
